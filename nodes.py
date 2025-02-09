54aaeffa9fa8709b1559bfd34790e648d56ea421
Ola Nordrum Isachsen <120105464+olanordrum@users.noreply.github.com>
2025-02-09 18:32:22 +0100

    Add NodeGroup class

--- nodes.py
+++ nodes.py
@@ -16,3 +16,39 @@ class Node(object):
                 line_end = self.neighbours[n].position.asTuple()
                 pygame.draw.line(screen, WHITE, line_start, line_end, 4)
                 pygame.draw.circle(screen, RED, self.position.asInt(), line_end, 12)
+                
+
+class NodeGroup(object):
+    def __init__(self):
+        self.nodeList = []
+        
+        
+    def setupTestNodes(self):
+        nodeA = Node(80,80)
+        nodeB = Node(160,80)
+        nodeC = Node(80,160)
+        nodeD = Node(160,160)
+        nodeE = Node(208,160)
+        nodeF = Node(80,320)
+        nodeG = Node(208,320)
+        nodeA.neighbors[RIGHT] = nodeB
+        nodeA.neighbors[DOWN] = nodeC
+        nodeB.neighbors[LEFT] = nodeA
+        nodeB.neighbors[DOWN] = nodeD
+        nodeC.neighbors[UP] = nodeA
+        nodeC.neighbors[RIGHT] = nodeD
+        nodeC.neighbors[DOWN] = nodeF
+        nodeD.neighbors[UP] = nodeB
+        nodeD.neighbors[LEFT] = nodeC
+        nodeD.neighbors[RIGHT] = nodeE
+        nodeE.neighbors[LEFT] = nodeD
+        nodeE.neighbors[DOWN] = nodeG
+        nodeF.neighbors[UP] = nodeC
+        nodeF.neighbors[RIGHT] = nodeG
+        nodeG.neighbors[UP] = nodeE
+        nodeG.neighbors[LEFT] = nodeF
+        self.nodeList = [nodeA, nodeB, nodeC, nodeD, nodeE, nodeF, nodeG]
+        
+    def render(self, screen):
+        for node in self.nodeList:
+            node.render(screen)
