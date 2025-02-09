f3f5d2bb98a7df0617647fb952e33a7bb4404bda
Ola Nordrum Isachsen <120105464+olanordrum@users.noreply.github.com>
2025-02-09 17:54:07 +0100

    Add existing project files to git

---
+++ run.py
@@ -0,0 +1,48 @@
+import pygame
+from pygame.locals import *
+from constants import *
+from pacman import Pacman 
+
+
+
+class GameColtroller(object):
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(SCREENSIZE,0,32)
+        self.background = None
+        self.clock = pygame.time.Clock()
+        
+        
+    def setBackground(self):
+        self.background = pygame.surface.Surface(SCREENSIZE)
+        self.background.fill(BLACK)
+        
+        
+    def startGame(self):
+        self.setBackground()
+        self.pacman = Pacman()
+        
+    def update(self):
+        dt = self.clock.tick(30) / 1000.0
+        self.pacman.update(dt)
+        self.checkEvents()
+        self.render()
+        
+    def checkEvents(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+    
+    
+    def render(self):
+        self.screen.blit(self.background,(0, 0))
+        self.pacman.render(self.screen)
+        pygame.display.update()
+        
+        
+
+if __name__ == "__main__":
+    game = GameColtroller()
+    game.startGame()
+    while True:
+        game.update()     
